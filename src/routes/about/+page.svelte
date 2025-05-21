2e6f2a4980a5aa7927a435ea20ae42250142db3b
Thanh Hung Le <thanh-hung.le@quanti.cz>
2025-05-21 21:20:34 +0200

    feat: history & about pages

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,22 @@
+<div class="flex flex-col items-center gap-4">
+  <h1 class="text-2xl font-bold">About</h1>
+
+  <p>
+    This is a simple decision-making app that helps you make choices by
+    providing a random yes or no answer. You can also keep track of your
+    decisions and their outcomes.
+  </p>
+
+  <p>
+    Your data is stored locally in your browser using IndexedDB, so you can
+    access it even when you're offline. 
+  </p>
+
+  <p>
+    The app is built with Svelte and SvelteKit. 
+    It uses the Dexie.js library for managing IndexedDB, making it easy to
+    store and retrieve your decisions. The app is designed to be simple and
+    user-friendly, with a clean interface that allows you to focus on making
+    decisions.
+  </p>
+</div>
---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import DecisionList from "@/lib/components/DecisionList.svelte";
+
+</script>
+
+<div class="flex flex-col items-center gap-4">
+  <h1 class="text-2xl font-bold">History</h1>
+
+  <DecisionList numberOfQuestions={100} />
+</div>
