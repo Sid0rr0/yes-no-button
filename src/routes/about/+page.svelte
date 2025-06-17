fc616a12e420989b38a5e4431d9e59659a9f7f1f
Thanh Hung Le <thanh-hung.le@quanti.cz>
2025-06-17 14:30:39 +0200

    feat: PWA install button (TODO fix)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,49 @@
+<script lang="ts">
+  import { Button } from "@/lib/components/ui/button"
+  import { onMount } from 'svelte'
+
+  interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: Array<string>;
+    readonly userChoice: Promise<{
+      outcome: 'accepted' | 'dismissed';
+      platform: string;
+    }>;
+    prompt(): Promise<void>;
+  }
+
+  onMount(() => {
+    // TODO - beforeinstallprompt fires before onMount
+    let installPrompt: BeforeInstallPromptEvent | null = null
+    const installButton = document.querySelector("#install")
+
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault()
+      installPrompt = event as BeforeInstallPromptEvent
+      installButton!.removeAttribute("hidden")
+    })
+
+    installButton!.addEventListener("click", async () => {
+      if (!installPrompt) {
+        return;
+      }
+
+      await installPrompt.prompt();
+      const userChoice = await installPrompt.userChoice;
+      console.log(`Install prompt was: ${userChoice.outcome}`);
+      disableInAppInstallPrompt();
+    });
+
+    function disableInAppInstallPrompt() {
+      installPrompt = null;
+      installButton!.setAttribute("hidden", "");
+    }
+
+    window.addEventListener("appinstalled", () => {
+      disableInAppInstallPrompt();
+    });
+  })
+</script>
+
 <div class="flex flex-col items-center gap-4 max-w-xl">
   <h1 class="text-2xl font-bold">About</h1>
 
@@ -10,7 +56,7 @@
   <h2 class="text-xl font-bold">Technical details</h2>
   <p>
     Your data is stored locally in your browser using IndexedDB and service workers, so you can
-    access it even when you're offline. 
+    access it even when you're offline. Also you can install this app on your device and use it as a PWA (Progressive Web App).
   </p>
 
   <p>
@@ -20,6 +66,8 @@
     user-friendly, with a clean interface that allows you to focus on making
     decisions.
   </p>
+
+  <Button id="install" hidden>Install this app</Button>
 
   <h2 class="text-xl font-bold">Links</h2>
   <div class="flex gap-4">
